57bff56b44d71c5146d50b27b85bf5ea27b9aa1e
Ankit Rohatgi <ankitrohatgi@hotmail.com>
2015-01-31 20:16:34 -0600

    add results from linux

---
+++ graphicsmagick/compile.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# 
+if [ -f main ]
+    then
+        rm main
+fi
+
+gcc -O3 -c graphicsmagick_c.c -I /usr/include/GraphicsMagick -pipe -Os -Wall -D_THREAD_SAFE -std=c99
+dmd -O -inline -release -c graphicsmagick.d
+dmd -O -inline -release -c main.d
+dmd -O -inline -release -c perf.d
+dmd -O -inline -release -ofmain main.o graphicsmagick.o graphicsmagick_c.o -L-L/opt/local/lib -L-lGraphicsMagick
+dmd -O -inline -release -ofperf perf.o graphicsmagick.o graphicsmagick_c.o -L-L/opt/local/lib -L-lGraphicsMagick
+rm *.o
