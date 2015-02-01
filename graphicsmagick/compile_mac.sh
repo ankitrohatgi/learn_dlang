450c01607957bc79d119cebb2f48f46221eb08cc
Ankit Rohatgi <ankitrohatgi@hotmail.com>
2015-01-31 18:00:45 -0600

    Is there a real need to link all those libraries?

--- graphicsmagick/compile_mac.sh
+++ graphicsmagick/compile_mac.sh
@@ -8,6 +8,5 @@ fi
 gcc -O3 -c graphicsmagick_c.c -I /opt/local/include/GraphicsMagick -pipe -Os -Wall -D_THREAD_SAFE
 dmd -O -inline -release -c graphicsmagick.d
 dmd -O -inline -release -c main.d
-dmd -O -inline -release -ofmain main.o graphicsmagick.o graphicsmagick_c.o -L-L/opt/local/lib -L-lGraphicsMagick -L-llcms2 -L-ltiff -L-lfreetype -L-ljasper -L-ljpeg -L-lpng16 -L-llzma -L-lbz2 -L-lxml2 -L-lz -L-lm -L-lpthread -L-lpng
-
+dmd -O -inline -release -ofmain main.o graphicsmagick.o graphicsmagick_c.o -L-L/opt/local/lib -L-lGraphicsMagick
 rm *.o
