a094bc675eb085b2abe0ff592e12e1adc0d0e037
Ankit Rohatgi <ankitrohatgi@hotmail.com>
2015-02-04 00:43:33 -0600

    small code to read json

--- json/readJSON.d
+++ json/readJSON.d
@@ -1,2 +1,19 @@
 import std.stdio;
+import std.json;
+import std.file;
 
+void main()
+{
+    // Read entire file
+    string jsonText = readText("test.json");
+    writeln(jsonText);
+
+    // Parse JSON
+    JSONValue json = parseJSON(jsonText);
+    writeln(json.toString());
+    writeln(json.object["c"].array[0].type);
+    writeln(json.object["c"].array[1].type);
+    writeln(json.object["c"].type);
+    writeln(json.type);
+    writeln(json.object["c"].array[0].str);
+}
